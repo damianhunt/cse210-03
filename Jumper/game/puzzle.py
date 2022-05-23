11f25d414c1df2b5f9dc79fd35cb134efe4222a6
damianhunt <dhuntsa@gmail.com>
2022-05-23 20:54:02 +0200

    commit

--- Jumper/game/puzzle.py
+++ Jumper/game/puzzle.py
@@ -1,22 +1,23 @@
-from random import Random, random
+from random import Random
 
-
-import random
 
 class Puzzle:
 
     def __init__(self):
-        self._list = ["animals, country, remotes, "]
+        self._list = ["animals", "country", "remotes", 'wares', 'soup', 'mount', 'extend',
+                    'brown', 'expert', 'tired', 'humidity', 'backpack', 'crust', 'dent', 
+                    'market', 'knock', 'smite', 'windy', 'coin', 'throw','silence', 'bluff', 
+                    'downfall', 'climb', 'lying', 'weaver', 'snob', 'kickoff', 'match', 'quaker',
+                    'foreman', 'excite', 'thinking', 'mend', 'allergen', 'pruning', 'coat',
+                    'emerald', 'coherent', 'manic', 'multiple', 'square','funded', 'funnel',
+                    'sailing', 'dream', 'mutation', 'strict', 'mystic', 'film', 'guide',
+                    'strain', 'bishop', 'settle', 'plateau', 'emigrate', 'marching', 'optimal',
+                    'medley', 'endanger', 'wick', 'condone', 'schema', 'rage', 'figure',
+                    'plague', 'aloof', 'there', 'reusable', 'refinery', 'suffer', 'affirm',
+                    'captive', 'flipping', 'prolong', 'main', 'coral', 'dinner', 'rabbit',
+                    'chill', 'seed',
+                    ]
 
-    def word(self):
-        list = self._list
-        random_word = Random.choice(list)
-        return random_word
-
-class Account:
-
-    def __init__(self):
-        self._transactions = [] # the "_" prefix means treat this as private
-        
-    def deposit(self, amount):
-        self._transactions.append(amount)
+    def get_word(self):
+        random_word = Random.choice(self._list)
+        return random_word.upper()
