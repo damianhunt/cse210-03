eee7e2ca7b773e686804c86636164835702b57df
damianhunt <dhuntsa@gmail.com>
2022-05-23 21:33:43 +0200

    commit

--- Jumper/game/parachute.py
+++ Jumper/game/parachute.py
@@ -1,64 +1,68 @@
-def display_hangman(tries):
-    stages = [  # final state: Parachute completely gone, player dies
-                """
-                 x
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """,
-                # another piece gone
-                """
-                \\/
-                 o
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """,
-                # another piece gone
-                """
-               \\  /
-                \\/
-                 o
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """,
-                # another piece gone
-                """
-                ___
-               \\  /
-                \\/
-                 o
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """,
-                # one piece off parachute
-                """
-               /___\\
-               \\  /
-                \\/
-                 o
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """,
-                # initial parachute state
-                """
-                ___
-               /___\\
-               \\  /
-                \\/
-                 o
-                /|\\
-                / \\
-                
-              ^^^^^^^  
-                """
-    ]
-    return stages[tries]
+
+class parachute:
+
+
+    def display_parachute(tries):
+        stages = [  # final state: Parachute completely gone, player dies
+                    """
+                     x
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """,
+                    # another piece gone
+                    """
+                    \\/
+                     o
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """,
+                    # another piece gone
+                    """
+                   \\  /
+                    \\/
+                     o
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """,
+                    # another piece gone
+                    """
+                    ___
+                   \\  /
+                    \\/
+                     o
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """,
+                    # one piece off parachute
+                    """
+                   /___\\
+                   \\  /
+                    \\/
+                     o
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """,
+                    # initial parachute state
+                    """
+                    ___
+                   /___\\
+                   \\  /
+                    \\/
+                     o
+                    /|\\
+                    / \\
+                    
+                  ^^^^^^^  
+                    """
+        ]
+        return stages[tries]
