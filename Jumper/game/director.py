7de89deb18233eada41ee7fc75b9e9540c951636
damianhunt <dhuntsa@gmail.com>
2022-05-25 19:27:26 +0200

    commit

--- Jumper/game/director.py
+++ Jumper/game/director.py
@@ -1,14 +1,14 @@
-from play import Play
-from puzzle import Puzzle
-from parachute import Parachute
+from .play import Play
+from .puzzle import Puzzle
+from .parachute import Parachute
 
 class Director:
     
-    def start_game():
-        word = Puzzle.get_word()
+    def start_game(self):
+        word = Puzzle.get_word(self)
         Play.play_game(word)
         while input("Play Again? (Y/N) ").upper() == "Y":
-            word = Puzzle.get_word()
+            word = Puzzle.get_word(self)
             Play.play_game(word)
         else:
             print("Game Over")
--- Jumper/game/play.py
+++ Jumper/game/play.py
@@ -1,32 +1,30 @@
-from parachute import Parachute
+from .parachute import Parachute
 
 class Play:
     def __init__(self):
-        
-        pass
+        self._word_completion = "_" * len(self)
+        self._guessed = False
+        self._guessed_letters = []
+        self._guessed_words = []
+        self._tries = 6
 
     def play_game(word):
-        word_completion = "_" * len(word)
-        guessed = False
-        guessed_letters = []
-        guessed_words = []
-        tries = 6
-        print("Let's play Hangman!")
+        print("Let's play Jumper!")
         print(Parachute.display_parachute(tries))
         print(word_completion)
         print("\n")
         while not guessed and tries > 0:
             guess = input("Please guess a letter or word: ").upper()
             if len(guess) == 1 and guess.isalpha():
-                if guess in guessed_letters:
+                if guess in Play.self._guessed_letters:
                     print("You already guessed the letter", guess)
                 elif guess not in word:
                     print(guess, "is not in the word.")
                     tries -= 1
-                    guessed_letters.append(guess)
+                    Play.self._guessed_letters.append(guess)
                 else:
                     print("Good job,", guess, "is in the word!")
-                    guessed_letters.append(guess)
+                    Play.self._guessed_letters.append(guess)
                     word_as_list = list(word_completion)
                     indices = [i for i, letter in enumerate(word) if letter == guess]
                     for index in indices:
@@ -35,12 +33,12 @@ class Play:
                     if "_" not in word_completion:
                         guessed = True
             elif len(guess) == len(word) and guess.isalpha():
-                if guess in guessed_words:
+                if guess in Play.self._guessed_words:
                     print("You already guessed the word", guess)
                 elif guess != word:
                     print(guess, "is not the word.")
                     tries -= 1
-                    guessed_words.append(guess)
+                    Play.self._guessed_words.append(guess)
                 else:
                     guessed = True
                     word_completion = word
