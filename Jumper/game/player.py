7f4f10634aadaea1d65f0c88368f9884cd4929a2
otieno-juma <103896399+otieno-juma@users.noreply.github.com>
2022-05-28 23:01:04 +0300

    Squashed commit of the following:
    commit 428e5cd4b2943bdd316dcebcc78ef3e4ffb47fe1
    Merge: 95eaca8 04042b0
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Fri May 27 19:55:41 2022 +0200
    
        Merge branch 'main' of https://github.com/damianhunt/cse210-03
    
    commit 95eaca8d8eb5b3c1b73e35c286ac0fc447e3ba10
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Fri May 27 19:53:46 2022 +0200
    
        commit
    
    commit 04042b0b7d5ccb86558c12e34de8348df967d2bd
    Author: otieno-juma <103896399+otieno-juma@users.noreply.github.com>
    Date:   Fri May 27 19:47:37 2022 +0300
    
        extra terminal deleted
    
    commit e1f4c659b9c46132e8f523d891a548d2364d38ba
    Author: rhodamabs <mab21004@byui.edu>
    Date:   Fri May 27 10:26:43 2022 +0200
    
        Commit
    
    commit d1bbcafe5d3c13964379ae810030148e3f242d05
    Author: rhodamabs <mab21004@byui.edu>
    Date:   Fri May 27 10:25:25 2022 +0200
    
        Commit
    
    commit 786905f81564f62764e5e1ae3742de25962abf00
    Author: rhodamabs <mab21004@byui.edu>
    Date:   Fri May 27 09:10:06 2022 +0200
    
        Commit
    
    commit 0256fa32b4071e1ac99a03eaa5444c9c3cdc33d6
    Author: rhodamabs <mab21004@byui.edu>
    Date:   Fri May 27 09:06:20 2022 +0200
    
        Commit
    
    commit 22016518cff475c5d5657c8e47396ea0da2c2ec7
    Merge: 11f25d4 2efbccf
    Author: Reace Ian Roeloffze <97016583+MadSoftwareEngineerReace@users.noreply.github.com>
    Date:   Wed May 25 20:51:12 2022 +0200
    
        Merge pull request #1 from damianhunt/Reace
    
        Reace
    
    commit 2efbccfa85d2673208effe8f29cd657871890cc4
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Wed May 25 20:44:44 2022 +0200
    
        commit
    
    commit b11a6faa9c1944267b819e0c032c3ffa46bc9350
    Author: MadSoftwareEngineerReace <righawkroeloffze@gmail.com>
    Date:   Wed May 25 20:42:43 2022 +0200
    
        Update
    
    commit 687ce14de8355149865486aaea61f70daa80a35a
    Author: MadSoftwareEngineerReace <righawkroeloffze@gmail.com>
    Date:   Wed May 25 20:36:34 2022 +0200
    
        update
    
    commit 6ca42de9fe9c099329d37a20f06efaed4bdccee1
    Author: MadSoftwareEngineerReace <righawkroeloffze@gmail.com>
    Date:   Wed May 25 20:34:02 2022 +0200
    
        Update
    
    commit bc49c5152eb957f3ea6c8bc5b9907d6d9b81a8c9
    Author: MadSoftwareEngineerReace <righawkroeloffze@gmail.com>
    Date:   Wed May 25 16:50:00 2022 +0200
    
        update
    
    commit 0673a2ed19720d20a765e15fc8509a605484597c
    Author: MadSoftwareEngineerReace <righawkroeloffze@gmail.com>
    Date:   Wed May 25 16:27:05 2022 +0200
    
        Editing Parachute game Code
    
    commit 11f25d414c1df2b5f9dc79fd35cb134efe4222a6
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Mon May 23 20:54:02 2022 +0200
    
        commit
    
    commit d0d0857da42449138fb0b6b7ab77a01495ff055e
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Mon May 23 20:45:29 2022 +0200
    
        commit
    
    commit 47c520d04ff8680295aa00e3109a3486d25eeaba
    Author: damianhunt <dhuntsa@gmail.com>
    Date:   Mon May 23 19:56:00 2022 +0200
    
        commit

---
+++ Jumper/game/player.py
@@ -0,0 +1,46 @@
+from Jumper.game.word import Word
+from parachute import Parachute
+from terminal_service import TerminalService
+
+class Game:
+    '''
+    The Director of the Game.
+    
+    Responsibility: Control the sequence/flow of Gameplay.
+    
+    Attributes:
+        game_in_progress (Boolean):
+            Whether or not the game is
+            currently being played or not.
+        Puzzle:
+            The word generator for the parachute game.
+        
+    '''
+    
+    def __init__(self):
+        
+        self._parachute = Parachute()
+        self._game_in_progress = True
+        self._guesses = False
+        self._word = Word()
+        self._terminal_service = TerminalService()
+       
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._game_in_progress:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+    
+    def _get_inputs(self):
+        """Moves the seeker to a new location.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        new_location = self._terminal_service.read_number("\nEnter a location [1-1000]: ")
+        self._seeker.move_location(new_location)
