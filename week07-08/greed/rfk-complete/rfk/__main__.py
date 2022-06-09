c8cb7a580bf6cbd3474c4016bfbc5b7ac32642df
Sydney Mangwana <man21063@byui.edu>
2022-06-09 06:52:04 +0200

    left&right movements only

--- week07-08/greed/rfk-complete/rfk/__main__.py
+++ week07-08/greed/rfk-complete/rfk/__main__.py
@@ -28,10 +28,10 @@ DEFAULT_ARTIFACTS = 40
 
 
 def main():
-    
+
     # create the cast
     cast = Cast()
-    
+
     # create the banner
     banner = Actor()
     banner.set_text("")
@@ -39,7 +39,7 @@ def main():
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
     cast.add_actor("banners", banner)
-    
+
     # create the robot
     x = int(MAX_X / 2)
     y = int(MAX_Y / 2)
@@ -51,7 +51,7 @@ def main():
     robot.set_color(WHITE)
     robot.set_position(position)
     cast.add_actor("robots", robot)
-    
+
     # create the artifacts
     with open(DATA_PATH) as file:
         data = file.read()
@@ -70,7 +70,7 @@ def main():
         g = random.randint(0, 255)
         b = random.randint(0, 255)
         color = Color(r, g, b)
-        
+
         artifact = Artifact()
         artifact.set_text(text)
         artifact.set_font_size(FONT_SIZE)
@@ -78,7 +78,7 @@ def main():
         artifact.set_position(position)
         artifact.set_message(message)
         cast.add_actor("artifacts", artifact)
-    
+
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
